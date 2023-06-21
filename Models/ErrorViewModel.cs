ed71d5d1186208e70efaeb450e66a15ff689d76f
clwanga <clwanga1095@gmail.com>
2023-06-21 14:40:06 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRVS_SG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
